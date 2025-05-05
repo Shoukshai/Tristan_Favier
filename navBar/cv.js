c4125fa8e67e358831f793f8514aeb0a54ff2a4f
Shoukshai <semilucky01@gmail.com>
2025-05-05 17:24:23 +0200

    fixed padding-right for the cv

--- navBar/cv.js
+++ navBar/cv.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let angle = 0;
     let speed = 1;
-    let acceleration = 0.02;
+    let acceleration = 0.05;
     let maxSpeed = 100;
   
     const image = document.getElementById("rotatingImage");
