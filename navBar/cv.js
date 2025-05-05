74adafc35ddaa9bb7e26b6f145a5529938a76095
Shoukshai <semilucky01@gmail.com>
2025-05-05 12:49:31 +0200

    Added the cv and other major changes Modified the cv so it's less intrusive Added rotation with acceleration and deceleration

---
+++ navBar/cv.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let angle = 0;
+    let speed = 1;
+    let acceleration = 0.02;
+    let maxSpeed = 100;
+  
+    const image = document.getElementById("rotatingImage");
+  
+    if (image) {
+      image.addEventListener("mouseenter", () => {
+        accelerating = true;
+      });
+  
+      image.addEventListener("mouseleave", () => {
+        accelerating = false;
+      });
+  
+      function rotate() {
+        angle += speed;
+        image.style.transform = `rotate(${angle}deg)`;
+  
+        if (image.matches(":hover") && speed < maxSpeed) {
+          speed += acceleration;
+        } else if (!image.matches(":hover") && speed > 1) {
+            speed -= acceleration;
+        }
+  
+        requestAnimationFrame(rotate);
+      }
+  
+      rotate();
+    } else {
+      console.error("Element with id 'rotatingImage' not found.");
+    }
+  });
+  
